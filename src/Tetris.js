32bd41ecfe17f72e33554795cd31195479b60987
briarwatson <briar.watson402@gmail.com>
2023-12-20 12:25:46 -0600

    TETRIS PROJECT NEW UPDATES

---
+++ src/Tetris.js
@@ -0,0 +1,35 @@
+import "Tetris.css"
+function Tetris () { 
+    return (
+        <div className="blackBackground">
+
+<body>
+    <div class="darkblueContainer">
+        <div class="box darkblue"></div>
+        <div class="box darkblue"></div>
+        <div class="box orange"></div>
+        <div class="box black"></div>
+        <div class="box green"></div>
+        <div class="box green"></div>
+        <div class="box red"></div>
+        <div class="box red"></div>
+        <div class="box purple"></div>
+        <div class="box black"></div>
+
+    </div>
+    <div class="darkblueContainer">
+        <div class="box darkblue"></div>
+        <div class="box darkblue"></div>
+        <div class="box orange"></div>
+        <div class="box orange"></div>
+        <div class="box orange"></div>
+        <div class="box green"></div>
+        <div class="box black"></div>
+        <div class="box red"></div>
+        <div class="box red"></div>
+        <div class="box orange"></div>
+    </div>
+</body>
+        </div>
+    )
+}
