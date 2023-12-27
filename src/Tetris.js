0ab0913ed236926ed4644e1c160a937c2511fd44
briarwatson <briar.watson402@gmail.com>
2023-12-26 18:53:29 -0600

    new moves

--- src/Tetris.js
+++ src/Tetris.js
@@ -1,9 +1,58 @@
+import { useState, useEffect } from "react";
 import "./Tetris.css"
-function Tetris () { 
+function Tetris () {
+    // Create a state variable called isRunning and set it to false when the page loads
+    // When they click the start button, we'll use setIsRunning to set it to true
+    const [isRunning, setIsRunning] = useState(false);
+    const [position, setPosition] = useState(0);
+    // The code in the useEffect will run whenever isRunning changes
+    useEffect(() => {
+        let interval;
+        if (isRunning) {
+            interval = setInterval(() => {
+                setPosition(prevPosition => {
+                    if (prevPosition >= 22) {
+                        clearInterval(interval);
+                        return prevPosition;
+                    }
+                    return prevPosition + 1;
+                });
+            }, 1000);
+        }
+        // This return runs when the page changes or unloads
+        return () => {
+            if (interval) {
+                clearInterval(interval);
+            }
+        };
+    }, [isRunning]);
+    function handleClick() {
+        setIsRunning(true);
+    }
+    
     return (
         <div className="blackBackground">
 
-<body>
+<div className="darkblueContainertwo" style={{ top: `${position * 30}px`}}>
+        <div className="box darkblue"></div>
+        <div className="box darkblue"></div>
+        <div className="box orange"></div>
+        <div className="box black"></div>
+        <div className="box green"></div>
+        <div className="box green"></div>
+        <div className="box red"></div>
+        <div className="box red"></div>
+        <div className="box purple"></div>
+        <div className="box black"></div>
+    </div>
+    {/* When isRunning is not true, render the start button */}
+    {!isRunning && 
+    <div class="startContainer">
+        <button onClick={handleClick} class="start">Start</button>
+    </div>  
+    }  
+    
+    <div>
     <div className="darkblueContainer">
         <div className="box darkblue"></div>
         <div className="box darkblue"></div>
@@ -17,6 +66,7 @@ function Tetris () {
         <div className="box black"></div>
 
     </div>
+    
     <div className="darkblueContainer">
         <div className="box darkblue"></div>
         <div className="box darkblue"></div>
@@ -29,8 +79,9 @@ function Tetris () {
         <div className="box red"></div>
         <div className="box orange"></div>
     </div>
-</body>
-        </div>
+</div>
+    </div>   
+   
     )
 }
 
